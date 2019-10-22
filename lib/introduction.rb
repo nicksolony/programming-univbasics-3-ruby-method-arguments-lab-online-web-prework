789dfdf794e57431cdab05549c4802f037c13c50
benefic-chain-7951 <nicksolony@gmail.com>
2019-10-22 01:23:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -17,11 +17,11 @@ end
 introduction_with_language ("Maria"), ("Ruby")
 
 
-def introduction_with_language(name, language)
+def introduction_with_language_optional (name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language ("Maria"), ("Ruby")
+introduction_with_language ("Alex")
 
 
 
